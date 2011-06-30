856ceeb5ed1ade90400360a840b1bca388004965
John Peele <john@rockbeatspaper.com>
2011-06-30 15:54:25 -0400

    Moved into the Skeleton CSS framework (ditched 960.gs), added @font-faces, began nav and dropdown menus

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,53 @@
-$(function() {
-  // Self-labeled form fields
+/* 
+* Skeleton V1.0.2
+* Copyright 2011, Dave Gamache
+* www.getskeleton.com
+* Free to use under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+* 5/20/2011
+*/	
+	
+
+$(document).ready(function() {
+  
+  /* Nav Dropdowns on hover
+	================================================== */
+  $('#primary-nav li').hover(
+      function() { $(this).addClass('hover').find('ul.dropdown').show(); },
+      function() { $(this).removeClass('hover').find('ul.dropdown').hide(); }
+  );
+
+	/* Tabs Activiation
+	================================================== */
+	var tabs = $('ul.tabs'),
+	    tabsContent = $('ul.tabs-content');
+	
+	tabs.each(function(i) {
+		//Get all tabs
+		var tab = $(this).find('> li > a');
+		tab.click(function(e) {
+			
+			//Get Location of tab's content
+			var contentLocation = $(this).attr('href') + "Tab";
+			
+			//Let go if not a hashed one
+			if(contentLocation.charAt(0)=="#") {
+			
+				e.preventDefault();
+			
+				//Make Tab Active
+				tab.removeClass('active');
+				$(this).addClass('active');
+				
+				//Show Tab Content
+				$(contentLocation).show().siblings().hide();
+				
+			} 
+		});
+	}); 
+	
+	/* Self-labeled form fields
+	================================================== */
 	$("input[title], textarea[title]").each(function() {
 		if($(this).val() === "") {
 			$(this).val($(this).attr("title"));	
@@ -15,5 +63,5 @@ $(function() {
 			}
 		});
 	});
-
-});	 
+	
+});
