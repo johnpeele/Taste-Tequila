a9c65ca281696574c5d34b3f07abd448e823e781
John Peele <john@rockbeatspaper.com>
2011-08-05 12:30:31 -0400

    Finished Brand Profile template

--- js/scripts.js
+++ js/scripts.js
@@ -12,46 +12,47 @@ $(document).ready(function() {
   
   /* Polyfills for CSS type selectors
 	================================================== */
-  // $("#sidebar .node:only-child").addClass("onlynode");
-  $("#sidebar .node:last-child").not("#sidebar .node:first-child").addClass("lastnode");
-  $("#sidebar .ratings-list li:last-child").addClass("last");
-  $("#sidebar .badge-list li:nth-child(5n)").addClass("last");
-  $("#sidebar .avatar-grid li:nth-child(5n)").addClass("last");
+  // $('#sidebar .node:only-child').addClass('onlynode');
+  $('#sidebar .node:last-child').not('#sidebar .node:first-child').addClass('lastnode');
+  $('#sidebar .ratings-list li:last-child').addClass('last');
+  $('#sidebar .badge-list li:nth-child(5n)').addClass('last');
+  $('#sidebar .avatar-grid li:nth-child(5n)').addClass('last');
+  $('.ratings span:last-child').addClass('last');
   
   /* Nav Dropdowns on hover
 	================================================== */
-  $("#primary-nav li").hover(
-      function() { $(this).addClass("hover").find("ul.dropdown").show(); },
-      function() { $(this).removeClass("hover").find("ul.dropdown").hide(); }
+  $('#primary-nav li').hover(
+    function() { $(this).addClass('hover').find('ul.dropdown').show(); },
+    function() { $(this).removeClass('hover').find('ul.dropdown').hide(); }
   );
   
   /* Tipsy Tooltips
 	================================================== */
-  $("[rel=tipsy]").tipsy({gravity: "w", opacity: 1});
-  $("[rel=tipsy-top]").tipsy({gravity: "s", opacity: 1});
-  $("[rel=tipsy-top-html]").tipsy({gravity: "s", html: "true", opacity: 1});
+  $('[rel=tipsy]').tipsy({gravity: 'w', opacity: 1});
+  $('[rel=tipsy-top]').tipsy({gravity: 's', opacity: 1});
+  $('[rel=tipsy-top-html]').tipsy({gravity: 's', html: 'true', opacity: 1});
 
 	/* Tabs Activiation
 	================================================== */
-	var tabs = $("ul.tabs"),
-	    tabsContent = $("ul.tabs-content");
+	var tabs = $('ul.tabs'),
+	    tabsContent = $('ul.tabs-content');
 	
 	tabs.each(function(i) {
 		//Get all tabs
-		var tab = $(this).find("> li > a");
+		var tab = $(this).find('> li > a');
 		tab.click(function(e) {
 			
-			//Get Location of tab"s content
-			var contentLocation = $(this).attr("href") + "Tab";
+			//Get Location of tab's content
+			var contentLocation = $(this).attr('href') + 'Tab';
 			
 			//Let go if not a hashed one
-			if(contentLocation.charAt(0)=="#") {
+			if(contentLocation.charAt(0)=='#') {
 			
 				e.preventDefault();
 			
 				//Make Tab Active
-				tab.removeClass("active");
-				$(this).addClass("active");
+				tab.removeClass('active');
+				$(this).addClass('active');
 				
 				//Show Tab Content
 				$(contentLocation).show().siblings().hide();
@@ -60,20 +61,27 @@ $(document).ready(function() {
 		});
 	}); 
 	
+	/* Brand Profile - show/hide categories
+	================================================== */
+	$('.group .singles').hide();
+	$('.group h3').click(function() {
+	  $(this).toggleClass('selected').parent().find('.singles').slideToggle();
+	});
+	
 	/* Self-labeled form fields
 	================================================== */
-	$("input[title], textarea[title]").each(function() {
-		if($(this).val() === "") {
-			$(this).val($(this).attr("title"));	
+	$('input[title], textarea[title]').each(function() {
+		if($(this).val() === '') {
+			$(this).val($(this).attr('title'));	
 		}
 
 		$(this).focus(function() {
-			if($(this).val() == $(this).attr("title")) {
-				$(this).val("").addClass("focused");	
+			if($(this).val() == $(this).attr('title')) {
+				$(this).val('').addClass('focused');	
 			}
 		}).blur(function() {
-			if($(this).val() === "") {
-				$(this).val($(this).attr("title")).removeClass("focused");	
+			if($(this).val() === '') {
+				$(this).val($(this).attr('title')).removeClass('focused');	
 			}
 		});
 	});
